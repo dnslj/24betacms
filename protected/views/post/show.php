f779aa49091fb846f5a85aacdc69e33dd06a224d
Dong Chen <cdcchen@gmail.com>
2012-08-14 13:22:19 +0800

    更换侧边栏广告为336x260

--- protected/views/post/show.php
+++ protected/views/post/show.php
@@ -37,10 +37,10 @@
     <div class="beta-block">
         <script type="text/javascript"><!--
             google_ad_client = "ca-pub-9725980429199769";
-            /* beta_300x250 */
-            google_ad_slot = "4422400660";
-            google_ad_width = 300;
-            google_ad_height = 250;
+            /* beta_336x280 */
+            google_ad_slot = "9661689878";
+            google_ad_width = 336;
+            google_ad_height = 280;
             //-->
         </script>
         <script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"></script>
