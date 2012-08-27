300b5b75c92ad567d80f11f1e4e74694aa9438d9
Dong Chen <cdcchen@gmail.com>
2012-08-27 15:09:34 +0800

    去掉无用的测试方法

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -138,23 +138,6 @@ class SiteController extends Controller
         );
     }
     
-    public function actionTop()
-    {
-        exit;
-        //CdTopApi::debug();
-        $api_key = '12228873';
-        $api_secret = '226deeb654bf7c233cb8e06886d7dd7d';
-        $sandbox_secret = 'sandbox654bf7c233cb8e06886d7dd7d';
-        $client = new CdTopApi($api_key, $api_secret, $sandbox_secret);
-        $data = $client->method('taobao.taobaoke.items.get')
-            ->add_args('fields', 'num_iid,title,nick,pic_url,price,click_url,commission,commission_rate,commission_num,commission_volume,shop_click_url,seller_credit_score,item_location,volume')
-            ->add_args('nick', 'cdcchencdc')
-            ->add_args('cid', '50006843')
-            ->request()->data();
-        print_r($client->error());
-        print_r($data);
-    }
-    
     public function actionTest()
     {
         phpinfo();
