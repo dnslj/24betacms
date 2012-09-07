a504b2d5cf5557ece5741ca9dd74994b31f067e0
Dong Chen <cdcchen@gmail.com>
2012-09-07 15:09:48 +0800

    update jquery lib to 1.8.0

--- protected/modules/mobile/views/default/index.php
+++ protected/modules/mobile/views/default/index.php
@@ -1,9 +1,9 @@
 <?php foreach ($models as $model):?>
 <dl class="post-item">
     <dt><h1><?php echo $model->titleLink;?></h1></dt>
-    <dt class="create-time"><?php echo $model->shortDate;?></dt>
     <dd class="clearfix">
         <?php echo $model->topicIconHtml;?><?php echo $model->filterSummary;?>
     </dd>
+    <dt class="extra"><?php echo $model->shortDate;?></dt>
 </dl>
 <?php endforeach;?>
