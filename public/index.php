69c3c2a4bb270fdb63c9abd90eff4386cfb7a82f
Dong Chen <cdcchen@gmail.com>
2013-02-06 14:32:56 +0800

    修改错误级别

--- public/index.php
+++ public/index.php
@@ -1,12 +1,9 @@
 <?php
+error_reporting(E_ALL ^ E_NOTICE);
 defined('BETA_PRODUCT') or define('BETA_PRODUCT', false);
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 define('BETA_WEBROOT', dirname(__FILE__));
 defined('DS') or define('DS', DIRECTORY_SEPARATOR);
-if (YII_DEBUG)
-     error_reporting(E_ALL);
-else
-    error_reporting(E_ALL ^ E_NOTICE);
 
 $bootstrap = extension_loaded('apc') ? 'yiilite.php' : 'yii.php';
 $yii = BETA_WEBROOT . '/../library/framework/' . $bootstrap;
